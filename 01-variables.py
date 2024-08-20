1b2ce29341218c375e35069af4c5638eb3cac64a
Ian Macharia <ianmachariak17@gmail.com>
2024-08-20 12:10:32 +0300

    feat: Covered variables

---
+++ 01-variables.py
@@ -0,0 +1,37 @@
+# Intro to different variables in Python
+
+# Integer - it is a whole number
+a = 1
+print("Integer: ", a)
+
+# Float - it is a decimal number
+b = 1.0
+print("Float: ", b)
+
+# String - it is a sequence of characters
+c = "hello"
+print("String: ", c)
+
+# Boolean - it is either True or False
+d = True
+print("Boolean: ", d)
+
+# List - it is a collection of items
+e = [1, 2, 3]
+print("List: ", e)
+
+# Dictionary - it is a collection of key-value pairs
+f = {"a": 1, "b": 2}
+print("Dictionary: ", f)
+
+# Tuple - it is a collection of items but unlike a list, it cannot be modified
+g = (1, 2, 3)
+print("Tuple: ", g)
+
+# Set - it is a collection of unique items
+h = {1, 2, 3}
+print("Set: ", h)
+
+# None - it is a special value that represents the absence of a value
+i = None
+print("None: ", i)
